cc6b5f426ad190acd46ac13c3bced0e7c9da8289
vkhalaim <vasyl.khalaim@ukr.net>
2020-02-13 16:45:32 +0200

    Completed 119 (39 %) koans and 15 (out of 37) lessons.

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,10 +18,16 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError(AttributeError('Negative sides isn\' allowed.'))
+
+    if a + b + c < 2 * max(a, b, c):
+        raise TriangleError(AttributeError('Sum of two sides should be less than third side.'))
     if a == b == c:
         return 'equilateral'
     elif a == b or a == c or b == c:
         return 'isosceles'
+
     return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
